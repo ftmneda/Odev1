fafdb79cd6264a62a710d51d2b6c5d8d581b506c
ftmdid <fnt0003@auburn.edu>
2023-10-25 10:33:40 +0300

    Initial Commit!

---
+++ asakSayiHesaplama/prod/asalKomsu.py
@@ -0,0 +1,8 @@
+'''
+Created on Oct 25, 2023
+
+@author: neda
+'''
+
+def asalSayi(ustSınır=None):
+    pass
---
+++ asakSayiHesaplama/test/asalKomsuTest.py
@@ -0,0 +1,22 @@
+'''
+Created on Oct 25, 2023
+
+@author: neda
+'''
+import unittest
+import prod.asalKomsu as ak
+
+
+class Test(unittest.TestCase):
+
+
+    def test010(self):
+        sonuc = ak.asalSayi(ustSınır=2)
+        beklenenSonuc= 2
+        self.assertEqual(beklenenSonuc, sonuc)
+        
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.test010']
+    unittest.main()
